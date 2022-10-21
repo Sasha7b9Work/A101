9e6403cc342500511bc8cba9e3c634d8ff6dd1bb
Sasha7b9Work <Phuntik-it@tut.by>
2022-10-21 17:09:41 +0300

    *

---
+++ sources/VS/Device/Hardware/HAL/HAL_TIM4_win.cpp
@@ -0,0 +1,27 @@
+// 2022/10/21 17:08:38 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Hardware/HAL/HAL.h"
+
+
+void HAL_TIM4::Init()
+{
+
+}
+
+
+void HAL_TIM4::Start(uint)
+{
+
+}
+
+
+void HAL_TIM4::WaitEvent()
+{
+
+}
+
+
+void HAL_TIM4::Stop()
+{
+
+}
