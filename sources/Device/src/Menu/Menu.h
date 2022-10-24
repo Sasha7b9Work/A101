eaaf809d4982cad7ea4c758786a36eeee9af1f85
Sasha7b9Work <Phuntik-it@tut.by>
2022-10-24 12:16:32 +0300

    *

--- sources/Device/src/Menu/Menu.h
+++ sources/Device/src/Menu/Menu.h
@@ -1,2 +1,5 @@
 // 2022/10/24 11:25:41 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
+
+
+
--- sources/Device/src/Menu/MenuItems.h
+++ sources/Device/src/Menu/MenuItems.h
@@ -1,3 +1,10 @@
 // 2022/10/24 11:25:49 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
+
+
+struct Button
+{
+public:
+    Button() {};
+};
