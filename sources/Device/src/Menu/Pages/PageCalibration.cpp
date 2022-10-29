a2bccb55414871c1ec7f5fba4e4cd31fb3c310c4
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-29 21:55:50 +0300

    *

--- sources/Device/src/Menu/Pages/PageCalibration.cpp
+++ sources/Device/src/Menu/Pages/PageCalibration.cpp
@@ -1,3 +1,59 @@
 // 2022/10/29 21:41:57 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Menu/Pages/PageCalibration.h"
+
+
+namespace PageCalibration
+{
+    static void FuncPress_0(Button *)
+    {
+
+    }
+
+    static Button button0("button0", "", false, FuncPress_0);
+
+
+    static void FuncPress_1(Button *)
+    {
+
+    }
+
+    static Button button1("button1", "", false, FuncPress_1);
+
+
+    static void FuncPress_2(Button *)
+    {
+
+    }
+
+    static Button button2("button2", "", false, FuncPress_2);
+
+
+    static void FuncPress_3(Button *)
+    {
+
+    }
+
+    static Button button3("button3", "", false, FuncPress_3);
+
+
+    static void FuncPress_4(Button *)
+    {
+
+    }
+
+    static Button button4("button4", "", false, FuncPress_4);
+
+
+    static void FuncPress_5(Button *)
+    {
+
+    }
+
+    static Button button5("button5", "", false, FuncPress_5);
+
+
+    static Page pageCalibration (&button0, &button1, &button2, &button3, &button4, &button5);
+
+    Page *self = &pageCalibration;
+}
--- sources/Device/src/Menu/Pages/PageDebug.cpp
+++ sources/Device/src/Menu/Pages/PageDebug.cpp
@@ -5,52 +5,52 @@
 
 namespace PageDebug
 {
-    static void FuncPress0(Button *)
+    static void FuncPress_0(Button *)
     {
 
     }
 
-    static Button button0("button0", "", false, FuncPress0);
+    static Button button0("button0", "", false, FuncPress_0);
 
 
-    static void FuncPress1(Button *)
+    static void FuncPress_1(Button *)
     {
 
     }
 
-    static Button button1("button1", "", false, FuncPress1);
+    static Button button1("button1", "", false, FuncPress_1);
 
 
-    static void FuncPress2(Button *)
+    static void FuncPress_2(Button *)
     {
 
     }
 
-    static Button button2("button2", "", false, FuncPress2);
+    static Button button2("button2", "", false, FuncPress_2);
 
 
-    static void FuncPress3(Button *)
+    static void FuncPress_3(Button *)
     {
 
     }
 
-    static Button button3("button3", "", false, FuncPress3);
+    static Button button3("button3", "", false, FuncPress_3);
 
 
-    static void FuncPress4(Button *)
+    static void FuncPress_4(Button *)
     {
 
     }
 
-    static Button button4("button4", "", false, FuncPress4);
+    static Button button4("button4", "", false, FuncPress_4);
 
 
-    static void FuncPress5(Button *)
+    static void FuncPress_5(Button *)
     {
 
     }
 
-    static Button button5("button5", "", false, FuncPress5);
+    static Button button5("button5", "", false, FuncPress_5);
 
 
     static Page pageDebug(&button0, &button1, &button2, &button3, &button4, &button5);
