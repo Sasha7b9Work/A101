9518c4b2f8042435c0b6aecee8c9c5252b7efa50
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-29 10:29:35 +0300

    refact

---
+++ sources/VS/Device/Measurer/AD7691_win.cpp
@@ -0,0 +1,15 @@
+// 2022/10/29 10:28:45 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Measurer/AD7691.h"
+
+
+void AD7691::Init()
+{
+
+}
+
+
+uint AD7691::ReadValue()
+{
+    return 0;
+}
