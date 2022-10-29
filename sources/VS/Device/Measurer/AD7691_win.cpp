96d8fe8900beb7f97456c7abf13b79e8729fce00
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-30 00:17:37 +0300

    *

--- sources/VS/Device/Measurer/AD7691_win.cpp
+++ sources/VS/Device/Measurer/AD7691_win.cpp
@@ -1,6 +1,7 @@
 // 2022/10/29 10:28:45 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Measurer/AD7691.h"
+#include <cstdlib>
 
 
 void AD7691::Init()
@@ -11,5 +12,5 @@ void AD7691::Init()
 
 int AD7691::ReadValue()
 {
-    return 0;
+    return std::rand() - 100000;
 }
