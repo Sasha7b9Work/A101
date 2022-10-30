30371b4d6a8daa4e2198bd8e1c3ab5973b321609
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-30 14:10:36 +0300

    *

--- sources/VS/Updater/main.cpp
+++ sources/VS/Updater/main.cpp
@@ -1,11 +1,37 @@
 // 2022/04/29 13:57:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
+#pragma warning(push)
+#pragma warning(disable : 4668)
+#include <iostream>
+#include <conio.h>
+#pragma warning(pop)
 
 
 using namespace std;
 
 
-void main()
-{
+static void WriteErrorMessage();
 
+
+int main(int argc, char *[])
+{
+    if (argc != 3)
+    {
+        WriteErrorMessage();
+
+        getch();
+
+        return -1;
+    }
+
+    getch();
+
+    return 0;
+}
+
+
+static void WriteErrorMessage()
+{
+    cout << "Invalid syntx. Usage:" << endl;
+    cout << "\t update101.exe com1 a101.bin" << endl;
 }
