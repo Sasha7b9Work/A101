d6559deccc0726ae129d3627dd75767300a1b40d
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-10-30 13:32:05 +0300

    добавлена заготовка для Updater

---
+++ sources/VS/Updater/defines.h
@@ -0,0 +1,11 @@
+// 2022/04/29 14:52:10 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+
+
+typedef const char *pchar;
+typedef unsigned char uchar;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint;
+typedef signed char int8;
---
+++ sources/VS/Updater/main.cpp
@@ -0,0 +1,9 @@
+// 2022/04/29 13:57:01 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+using namespace std;
+
+
+void main()
+{
+
+}
