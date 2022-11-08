8fd1f5e4bcf0b9ebcf40eb9708b0f4321cfaa731
Sasha7b9Work <Phuntik-it@tut.by>
2022-11-08 13:03:14 +0300

    *

---
+++ sources/Device/src/Ampermeter/Calculator/FinderDC.cpp
@@ -0,0 +1,3 @@
+// 2022/11/08 13:02:19 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Ampermeter/Calculator/FinderDC.h"
---
+++ sources/Device/src/Ampermeter/Calculator/FinderDC.h
@@ -0,0 +1,2 @@
+// 2022/11/08 13:02:15 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
