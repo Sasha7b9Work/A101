24eaf4a2015244942fcb456f318da294a659c290
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2022-11-10 22:30:36 +0300

    *

---
+++ sources/Device/src/Display/Controls/TextMeasure.cpp
@@ -0,0 +1,3 @@
+// 2022/11/10 22:25:51 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Display/Controls/TextMeasure.h"
---
+++ sources/Device/src/Display/Controls/TextMeasure.h
@@ -0,0 +1,23 @@
+// 2022/11/10 22:26:12 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+class TextMeasure
+{
+public:
+private:
+};
+
+
+class TextMeasureAC : public TextMeasure
+{
+public:
+private:
+};
+
+
+class TextMeasureDC : public TextMeasure
+{
+public:
+private:
+};
