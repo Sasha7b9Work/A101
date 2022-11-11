ad32f7dbc37938b9c0e57ad68442e8cd753b5823
Sasha7b9Work <Phuntik-it@tut.by>
2022-11-11 16:08:23 +0300

    *

--- sources/VS/Emulator/Nextion/Nextion_win.cpp
+++ sources/VS/Emulator/Nextion/Nextion_win.cpp
@@ -31,7 +31,7 @@ void Nextion::DrawLine(int x1, int y1, int x2, int y2, const Color &color)
 }
 
 
-void Nextion::DrawString(int x, int y, int width, int height, int font, const Color &color, const Color &back_color, pchar text)
+void Nextion::DrawString(int x, int y, int width, int height, int font, const Color &color, const Color &back_color, pchar text, int)
 {
     FillRect(x, y, width, height, back_color);
 
