b0a470595623e67b56a4bd39007cab9bc5f27b92
Sasha7b9Work <Phuntik-it@tut.by>
2022-11-14 08:59:28 +0300

    Не зависает, хоть и не показывает цифры

--- sources/Device/src/Ampermeter/AD7691.cpp
+++ sources/Device/src/Ampermeter/AD7691.cpp
@@ -162,7 +162,7 @@ ValueADC::ValueADC(int reading)
         value -= 1 << 18;
     }
 
-    static const int delta[6] = { 3290, 3254, 3250, 3254, 3290, 3290 };
+    static const int delta[6] = { 3291, 3254, 3250, 3254, 3290, 3290 };
 
     value -= delta[InputRelays::GetRange()];
 }
