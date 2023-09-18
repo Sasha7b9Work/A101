b960d50a28baf87b93c5e2e70c0d0b728fcd84dc
Sasha7b9Work <Phuntik-it@tut.by>
2023-09-18 13:24:38 +0300

    *

--- sources/Device/src/Menu/MenuItems.h
+++ sources/Device/src/Menu/MenuItems.h
@@ -8,8 +8,8 @@ struct Button
 public:
 
     // _highlight - в этом состоянии кнопка находится при первом появлении на экране
-    Button(pchar _name, void (*_funcOnPress)(Button *), void (*_funcOnInit)(Button *) = EmptyFuncInit) :
-        name(_name), funcOnPress(_funcOnPress), funcOnInit(_funcOnInit)
+    Button(void (*_funcOnPress)(Button *), void (*_funcOnInit)(Button *) = EmptyFuncInit) :
+        name(""), funcOnPress(_funcOnPress), funcOnInit(_funcOnInit)
     {
     }
 
--- sources/Device/src/Menu/Pages/PageMain.cpp
+++ sources/Device/src/Menu/Pages/PageMain.cpp
@@ -6,17 +6,17 @@
 
 namespace PageMain
 {
-    static Button button2mA("", [](Button *) { });
+    static Button button2mA([](Button *) { });
 
-    static Button button20mA("", [](Button *) { });
+    static Button button20mA([](Button *) { });
 
-    static Button button200mA("", [](Button *) { });
+    static Button button200mA([](Button *) { });
 
-    static Button button2A("", [](Button *) { });
+    static Button button2A([](Button *) { });
 
-    static Button button20A("", [](Button *) { });
+    static Button button20A([](Button *) { });
 
-    static Button button50A("", [](Button *) { });
+    static Button button50A([](Button *) { });
 
 
     static Page pageMain(&button2mA, &button20mA, &button200mA, &button2A, &button20A, &button50A);
