7c348337d0cd27131399c22172e541c0abd8f6c6
Sasha7b9Work <Phuntik-it@tut.by>
2023-09-18 14:34:00 +0300

    Заготовки для кнопок

--- sources/Device/src/Menu/Pages/PageCalibration.cpp
+++ sources/Device/src/Menu/Pages/PageCalibration.cpp
@@ -6,26 +6,77 @@
 
 namespace PageCalibration
 {
-    static Button button0("", [](Button *) {});
+    static Button btnBack("2B0", [](Button *) {});
 
-    static Button button1("", [](Button *) {});
+    static Button btnSave("2SV", [](Button *) {});
 
-    static Button button2("", [](Button *) {});
+    static Button btnOK("2OK", [](Button *) {});
 
-    static Button button3("", [](Button *) {});
+    static Button btnDot1("2D1", [](Button *) {});
 
-    static Button button4("", [](Button *) {});
+    static Button btnDot2("2D2", [](Button *) {});
 
-    static Button button5("", [](Button *) {});
+    static Button btn2mA("21P", [](Button *) {});
+
+    static Button btn20mA("22P", [](Button *) {});
+
+    static Button btn200mA("23P", [](Button *) {});
+
+    static Button btn2A("24P", [](Button *) {});
+
+    static Button btn20A("25P", [](Button *) {});
+
+    static Button btn50A("26P", [](Button *) {});
+
+    static Button btn0("KB0", [](Button *) {});
+
+    static Button btn1("KB1", [](Button *) {});
+
+    static Button btn2("KB2", [](Button *) {});
+
+    static Button btn3("KB3", [](Button *) {});
+
+    static Button btn4("KB4", [](Button *) {});
+
+    static Button btn5("KB5", [](Button *) {});
+
+    static Button btn6("KB6", [](Button *) {});
+
+    static Button btn7("KB7", [](Button *) {});
+
+    static Button btn8("KB8", [](Button *) {});
+
+    static Button btn9("KB9", [](Button *) {});
+
+    static Button btnDot("KBD", [](Button *) {});
+
+    static Button btnSign("KBS", [](Button *) {});
 
     static Button *buttons[] =
     {
-        &button0,
-        &button1,
-        &button2,
-        &button3,
-        &button4,
-        &button5,
+        &btnBack,
+        &btnSave,
+        &btnOK,
+        &btnDot1,
+        &btnDot2,
+        &btn2mA,
+        &btn20mA,
+        &btn200mA,
+        &btn2A,
+        &btn20A,
+        &btn50A,
+        &btn0,
+        &btn1,
+        &btn2,
+        &btn3,
+        &btn4,
+        &btn5,
+        &btn6,
+        &btn7,
+        &btn8,
+        &btn9,
+        &btnDot,
+        &btnSign,
         nullptr
     };
 
