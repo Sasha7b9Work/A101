3e6e2961f43de642cd35d9ac9faf2ae9b4ff4d0f
Sasha7b9Work <Phuntik-it@tut.by>
2023-09-27 15:46:30 +0300

    *

--- sources/Device/src/Menu/Pages/PageCalibration.cpp
+++ sources/Device/src/Menu/Pages/PageCalibration.cpp
@@ -144,30 +144,9 @@ namespace PageCalibration
 
     static Button *buttons[] =
     {
-        &btnBack,
-        &btnSave,
-        &btnCalib,
-        &btnMin,
-        &btnMax,
-        &btn2mA,
-        &btn20mA,
-        &btn200mA,
-        &btn2A,
-        &btn20A,
-        &btn50A,
-        &btn0,
-        &btn1,
-        &btn2,
-        &btn3,
-        &btn4,
-        &btn5,
-        &btn6,
-        &btn7,
-        &btn8,
-        &btn9,
-        &btnDot,
-        &btnSign,
-        nullptr
+        &btnBack, &btnSave, &btnCalib, &btnMin, &btnMax,  &btn2mA, &btn20mA, &btn200mA,
+        &btn2A,   &btn20A,  &btn50A,   &btn0,   &btn1,    &btn2,   &btn3,    &btn4,
+        &btn5,    &btn6,    &btn7,     &btn8,   &btn9,    &btnDot, &btnSign, nullptr
     };
 
     namespace ButtonsRange
