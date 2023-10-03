1ba1f21180f4c35105db3415bc6692183ebb5394
Sasha7b9Work <Phuntik-it@tut.by>
2023-10-03 15:02:25 +0300

    detrash

--- sources/Device/src/Ampermeter/Calibrator/Calibrator.cpp
+++ sources/Device/src/Ampermeter/Calibrator/Calibrator.cpp
@@ -51,9 +51,6 @@ namespace Calibrator
 
     static bool event_run = false;
 
-    // level - 0: 0mA, 1 - верхний уровень
-    static void DrawPromt(int range, int level);
-
     static void ProcedureCalibrate(int range, int level);
 
     static void CalibrateHardware(int range, int level);
@@ -145,8 +142,6 @@ void Calibrator::DrawParameters()
 
 void Calibrator::ProcedureCalibrate(int range, int level)
 {
-    DrawPromt(range, level);
-
     if (event_run)
     {
         Nextion::Button::Disable("buttonOk");
@@ -286,39 +281,6 @@ void Calibrator::TimeLine::Draw()
 
         color = (color.value == Color::White.value) ? Color::Background : Color::White;
     }
-}
-
-
-void Calibrator::DrawPromt(int range, int level)
-{
-    const int height = 40;
-    const int width = 550;
-    const int delta = 60;
-    const int x = 140;
-
-    int y = 30;
-
-    static const char *ranges[6] = { "2 mA", "20 mA", "200 mA", "2 A", "20 A", "50 A" };
-
-    char buffer[50] = { '\0' };
-
-    std::sprintf(buffer, "Калибровка диапазона %s.", ranges[range]);
-
-    Nextion::DrawString(10, y, 780, height, 2, Color::White, Color::Background, buffer, 1);
-
-    y += delta + delta / 2;
-
-    Nextion::DrawString(x, y, width, height, 2, Color::White, Color::Background, "Подайте на вход амперметра");
-
-    y += delta;
-
-    std::sprintf(buffer, "постоянный ток величиной %s", level == 0 ? "0 mA" : ranges[range]);
-
-    Nextion::DrawString(x, y, width + 20, height, 2, Color::White, Color::Background, buffer);
-
-    y += delta;
-
-    Nextion::DrawString(x, y, width, height, 2, Color::White, Color::Background, "и нажмите кнопку <Готово>.");
 }
 
 
