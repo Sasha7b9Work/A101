15bd272c68d4748fc59828b70183851604e2afcf
Sasha7b9Work <Phuntik-it@tut.by>
2023-11-15 15:05:33 +0300

    *

--- sources/VS/A101-GUI/Communicator/ComPort.cpp
+++ sources/VS/A101-GUI/Communicator/ComPort.cpp
@@ -1,6 +1,7 @@
 // (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Communicator/ComPort.h"
+#include "SCPI/Parser.h"
 #undef CRC
 #include "Communicator/rs232.h"
 #include <cstring>
@@ -15,20 +16,27 @@ bool ComPort::Open()
 
     uint8 buffer[4096];
 
-    uint8 message[] = "Test string";
+    pchar message = "*IDN?\r\n";
 
     for (int i = 0; i < 30; i++)
     {
         if (RS232_OpenComport(i, 115200, mode, 0) == 0)
         {
-            RS232_SendBuf(i, message, 5);
+            RS232_SendBuf(i, (uint8 *)message, (int)std::strlen(message));
 
             int n = RS232_PollComport(i, buffer, 4095);
 
             if (n > 0)
             {
-                openedPort = i;
-                return true;
+                BufferParser pars_buf;
+
+                Parser::GetWord((char *)buffer, 3, &pars_buf);
+
+                if (std::strcmp(pars_buf.data, "A101") == 0)
+                {
+                    openedPort = i;
+                    return true;
+                }
             }
 
             RS232_CloseComport(i);
