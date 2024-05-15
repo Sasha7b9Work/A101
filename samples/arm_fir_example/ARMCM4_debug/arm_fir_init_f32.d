a787aaad6a26444dd5319eb13c2cd1c8b810ab99
Sasha7b9Work <Phuntik-it@tut.by>
2024-05-15 08:38:39 +0300

    *

---
+++ samples/arm_fir_example/ARMCM4_debug/arm_fir_init_f32.d
@@ -0,0 +1,12 @@
+./armcm4_debug/arm_fir_init_f32.o: \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\DSP\Source\FilteringFunctions\arm_fir_init_f32.c \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\DSP\Include\arm_math.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
---
+++ samples/arm_fir_example/ARMCM4_debug/system_stm32f4xx.d
@@ -0,0 +1,12 @@
+./armcm4_debug/system_stm32f4xx.o: system_stm32f4xx.c \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_version.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Include\mpu_armv7.h \
+  ..\common\libs\STM32Cube_F4\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
