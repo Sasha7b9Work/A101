613ffd980f213d3b067b5f9ec3d7a782140d7010
Sasha7b9Work <Sasha7b9@tut.by>
2024-08-21 09:44:45 +0300

    Сделаны три дополнительных измерения, но на последних трёх приделах пик-то-пик неправильно показывает

--- sources/Device/src/defines.h
+++ sources/Device/src/defines.h
@@ -4,9 +4,9 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_BUILD 106
+#define VERSION_BUILD 107
 
-#define DATE_BUILD "2024-08-21 09:44:35"
+#define DATE_BUILD "2024-08-21 09:44:45"
 
 #define VERSION_SCPI  "1.0"
 
