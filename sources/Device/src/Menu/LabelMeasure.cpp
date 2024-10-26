4b8bdf8e6c2d52dbd3adf86378a4a644269a0ca4
Sasha7b9Work <Sasha7b9@tut.by>
2024-10-26 15:00:48 +0300

    При перегрузке частота показывает ****

--- sources/Device/src/Menu/LabelMeasure.cpp
+++ sources/Device/src/Menu/LabelMeasure.cpp
@@ -250,7 +250,7 @@ void LabelMeasure::SetMeasure(const Measure &measure, int range)
     {
         if (measure.out_of_range)
         {
-            std::strcpy(buf_measure, " OL");
+            std::strcpy(buf_measure, type_measure.IsFrequency() ? "*****" : " OL");
         }
         else
         {
