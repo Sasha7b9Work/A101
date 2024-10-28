92e904a4048b6585dd56f62d650183c248bf86d8
Sasha7b9Work <Sasha7b9@tut.by>
2024-10-28 09:44:06 +0300

    Оставили на всякий случай графики

--- sources/Device/src/defines.h
+++ sources/Device/src/defines.h
@@ -4,9 +4,9 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_BUILD 455
+#define VERSION_BUILD 456
 
-#define DATE_BUILD "2024-10-28 09:40:16"
+#define DATE_BUILD "2024-10-28 09:44:06"
 
 #define VERSION_SCPI  "1.0"
 
