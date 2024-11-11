8a90c21d5cc1136e47068b1abb5063d9c38aef17
Sasha7b9Work <Sasha7b9@tut.by>
2024-11-11 10:48:36 +0300

    *

--- sources/Device/src/defines.h
+++ sources/Device/src/defines.h
@@ -4,9 +4,9 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_BUILD 466
+#define VERSION_BUILD 467
 
-#define DATE_BUILD "2024-10-28 15:37:45"
+#define DATE_BUILD "2024-11-11 10:48:36"
 
 #define VERSION_SCPI  "1.0"
 
