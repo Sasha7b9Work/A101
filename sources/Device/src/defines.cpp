bf3be658453b4ab908836390c66acc88f866005b
Sasha7b9Work <Sasha7b9@tut.by>
2024-11-27 11:57:27 +0300

    detrash

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 516
+#define VERSION_BUILD 517
 
-#define DATE_BUILD "2024-11-27 11:57:20"
+#define DATE_BUILD "2024-11-27 11:57:27"
 
 #define VERSION_SCPI  "1.0"
 
