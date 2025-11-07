85d3f7fcc0238601e2f248a1356e07d696825b8f
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-07 11:34:55 +0300

    Восстановлена работоспособность win-проектов

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 573
+#define VERSION_BUILD 574
 
-#define DATE_BUILD "2025-11-07 10:53:33"
+#define DATE_BUILD "2025-11-07 11:34:55"
 
 #define VERSION_SCPI  "1.0"
 
